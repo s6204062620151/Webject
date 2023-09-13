e47e2e6d8b7831a863320b9387b0f76af353bb36
unknown <s6404062620109@email.kmutnb.ac.th>
2023-09-13 20:19:39 +0700

    ux navbar responsive mobile,tablet,desktop.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import style from './App.module.css';
+import Navbar from './Components/Navbar';
+
+import Home from './Components/Home';
+import Status from './Components/Status';
+import Cart from './Components/Cart';
+import Signin from './Components/SignIn';
+import Signup from './Components/SignUp';
+import Category1 from './Components/Category1';
+import Category2 from './Components/Category2';
+import Category3 from './Components/Category3';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.container}>
+      <div className={style.navbar}><Navbar/></div>
+      <div className={style.content}>
+        <Routes>
+          <Route path="/" Component={Home}/>
+          <Route path="/Status" Component={Status}/>
+          <Route path="/Cart" Component={Cart}/>
+          <Route path="/Signin" Component={Signin}/>
+          <Route path="/Signup" Component={Signin}/>
+          <Route path="/categ1" Component={Category1}/>
+          <Route path="/categ2" Component={Category2}/>
+          <Route path="/categ3" Component={Category3}/>
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import style from './navbar.module.css'
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+  return (
+    <nav>
+        <ul className={style.Navmenu}>
+            <li className={style.categoryMenu} onClick={toggleDropdown}>
+                categories
+            </li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/Status">Status</Link></li>
+            <li><Link to="/Cart">Cart</Link></li>
+            <li><Link to="/SignIn">Sign In | Sign Up</Link></li>
+        </ul>
+        {isOpen && (
+          <ul className={style.dropdownmenu}>
+            <li><Link to="/categ1">Category1</Link></li>
+            <li><Link to="/categ2">Category2</Link></li>
+            <li><Link to="/categ3">Category3</Link></li>
+          </ul>
+        )}
+    </nav>
+  )
+}
+
+export default Navbar
